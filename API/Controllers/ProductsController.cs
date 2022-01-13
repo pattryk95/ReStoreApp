496353153766a903f3a5699807c7e636e8521af3
Patryk <Patryk@DESKTOP-25DF3S2>
2022-01-14 00:48:29 +0100

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")] // ex. api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
