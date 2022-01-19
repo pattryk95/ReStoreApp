bcc5ec15cb59053048cbac9fabe33e7cbdbbf228
Patryk <patryk.bogdanski95@gmail.com>
2022-01-20 00:54:46 +0100

    Add BasketItem and Basket Entity (and DbSet in DbContext)

---
+++ API/Entites/Basket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Entites
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int quantity)
+        {
+            if (Items.All(Items => Items.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
+            }
+
+            var exstingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if (exstingItem != null) exstingItem.Quantity += quantity;
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+            if (item == null) return;
+            item.Quantity -= quantity;
+            if (item.Quantity == 0) Items.Remove(item);
+            {
+
+            }
+        }
+    }
+}
---
+++ API/Entites/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entites
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
